32d135fd06ec19c9a162608bf4918a323c389420
Matheus Henrique <matheusemail222@gmail.com>
2024-12-20 10:02:42 -0300

    Add files via upload

---
+++ JAVASCRIPT/ex08.js
@@ -0,0 +1,11 @@
+var a = 58
+if (a > 60){
+    console.log(`A velocidade permitida é de 60Km/h você passou a ${a}Km/h você foi multado!!`)
+}
+
+ else {
+    console.log(`A velocidade do seu carro é de ${a}KM`)
+    console.log(`Dirija com segurança`)
+
+}
+
